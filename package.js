57887938a1852cc59d356c31f786b926b2870255
Matthew Simo <matthew.a.simo@gmail.com>
2014-02-16 17:29:03 -0600

    Update the npm node-github version to latest, 0.1.13

--- package.js
+++ package.js
@@ -2,7 +2,7 @@ Package.describe({
   summary: 'NodeJS wrapper for the GitHub API'
 });
 
-Npm.depends({github: '0.1.10'});
+Npm.depends({github: '0.1.13'});
 
 Package.on_use(function (api) {
   api.export('GitHub');
